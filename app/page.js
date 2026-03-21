4a340bed5368c1c2376dcf054f9361bca2b61bde
pwa1163000-bit <pwa1163000@gmail.com>
2026-03-21 21:58:05 +0700

    Enhance Home component with additional links
    Added a link to view job listings and improved layout.

--- app/page.js
+++ app/page.js
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <div>
       <h1>ระบบแจ้งซ่อม 💧</h1>
-      <a href="/create-job">แจ้งงาน</a>
+
+      <a href="/create-job">➕ แจ้งงาน</a>
+      <br /><br />
+      <a href="/jobs">📋 ดูรายการงาน</a>
     </div>
   );
 }
