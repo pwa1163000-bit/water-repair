7d9713ad8d0f46939bbad61c8500190f144dfb3d
pwa1163000-bit <pwa1163000@gmail.com>
2026-03-22 18:59:36 +0700

    Update MapPicker.js.js

---
+++ components/MapPicker.js.js
@@ -0,0 +1,23 @@
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const icon = L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png", iconSize: [25, 41], iconAnchor: [12, 41] });
+
+export default function MapPicker({ location, setLocation }) {
+  function LocationMarker() {
+    useMapEvents({
+      click(e) {
+        setLocation({ lat: e.latlng.lat, lng: e.latlng.lng });
+      },
+    });
+    return <Marker position={[location.lat, location.lng]} icon={icon} />;
+  }
+
+  return (
+    <MapContainer center={[location.lat, location.lng]} zoom={15} style={{ height: "100%", width: "100%" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <LocationMarker />
+    </MapContainer>
+  );
+}
